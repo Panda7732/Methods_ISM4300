4d66894551a71a650bee46b1b2ac569e32a1134b
Amanda Hernandez <amandah2@usf.edu>
2021-01-30 17:12:52 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ * Name:Amanda Hernandez
+ * Date:1/30/2021
+ * Description: Deliverabel 3-Methods: C# Console application that asks the user for their name and prints back "Hello 'Name.'"
+ */
+using System;
+
+namespace Methods_ISM4300
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask user what their name is for user input
+            Console.WriteLine("What is your name?");
+            //call method
+            Name();
+          
+        }
+        //method to display "Hello 'User name'!"
+        private static string Name()
+        {
+
+            //declare variable name and read input name
+            String input = Console.ReadLine();
+
+            //Display message "Hello 'user name'!"
+            Console.WriteLine("Hello" + " " + input + "!");
+
+            return input;
+        }
+
+}
+}
+;
